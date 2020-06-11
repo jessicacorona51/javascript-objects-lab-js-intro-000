c24db749ce74b124b01acb708cdf3f7472f81c59
Jessica Corona <jessicacorona51@yahoo.com>
2020-06-11 18:58:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function deleteFromObjectByKey (object, key) {
   delete clone[key];
   return clone;
 }
+function destructivelyDeleteFromOjectByKey (object, key) {
+  delete object[key];
+  return object;
+}
